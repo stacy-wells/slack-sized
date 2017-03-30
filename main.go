29fa0aa70e3058d4f29f1e14c4d9c4602e6d5dcf
Stacy Wells <stacy.wells@americastestkitchen.com>
2017-03-29 20:27:56 -0400

    add placeholder image, remove sha and cookie file system

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"crypto/sha1"
 	"fmt"
 	"html/template"
 	"image/jpeg"
@@ -10,13 +9,17 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/nfnt/resize"
-	"github.com/satori/go.uuid"
 )
 
+var i photo
+var path string
 var tpl *template.Template
+
+type photo struct {
+	Name string
+}
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
@@ -30,67 +33,38 @@ func main() {
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	c := getCookie(w, req)
 	if req.Method == http.MethodPost {
 		mf, fh, err := req.FormFile("nf")
 		if err != nil {
 			fmt.Println(err)
 		}
 		defer mf.Close()
-		// create sha for file name
-		ext := strings.Split(fh.Filename, ".")[1]
-		h := sha1.New()
-		io.Copy(h, mf)
-		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+
 		// create new file
-		wd, err := os.Getwd()
-		if err != nil {
-			fmt.Println(err)
-		}
-		path := filepath.Join(wd, "public", "pics", fname)
+		path = filepath.Join("public", "pics", fh.Filename)
 		nf, err := os.Create(path)
 		if err != nil {
 			fmt.Println(err)
 		}
 		defer nf.Close()
+
 		// copy
 		mf.Seek(0, 0)
 		io.Copy(nf, mf)
 
 		transformImage(path)
-		// add filename to this user's cookie
-		c = appendValue(w, c, fname)
 	}
-	xs := strings.Split(c.Value, "|")
-	// sliced cookie values to only send over images
-	tpl.ExecuteTemplate(w, "index.gohtml", xs[len(xs)-1])
-}
 
-func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
-	c, err := req.Cookie("session")
-	if err != nil {
-		sID := uuid.NewV4()
-		c = &http.Cookie{
-			Name:  "session",
-			Value: sID.String(),
-		}
-		http.SetCookie(w, c)
+	// create struct for view
+	i := photo{
+		Name: path,
 	}
-	return c
-}
 
-func appendValue(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
-	s := c.Value
-	if !strings.Contains(s, fname) {
-		s += "|" + fname
-	}
-	c.Value = s
-	http.SetCookie(w, c)
-	return c
+	tpl.ExecuteTemplate(w, "index.gohtml", i)
 }
 
 func transformImage(f string) {
-	// open "test.jpg"
+	// open uploaded file
 	file, err := os.Open(f)
 	if err != nil {
 		log.Fatal(err)
@@ -103,8 +77,7 @@ func transformImage(f string) {
 	}
 	file.Close()
 
-	// resize to width 128 using Lanczos resampling
-	// and preserve aspect ratio
+	// resize to width 128
 	m := resize.Resize(128, 128, img, resize.Lanczos3)
 
 	out, err := os.Create(f)
